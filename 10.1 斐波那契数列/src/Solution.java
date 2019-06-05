d9ef5e074bb40c4c914a86ccdd6b1af911d360c5
XFour <837065279@qq.com>
2019-06-05 13:03:39 +0800

    10.1 斐波那契数列

---
+++ 10.1 斐波那契数列/src/Solution.java
@@ -0,0 +1,28 @@
+/**
+ * Create with 10.1 斐波那契数列
+ * User: 许清远
+ * Date: 2019/6/5
+ * Time: 13:00
+ * Description:斐波那契数列的第 n 项，n <= 39。
+ *
+ * 解题思路：递归
+ */
+public class Solution {
+
+    public int Fibonacci(int n) {
+
+        if (n == 1) {
+            return 1;
+        }
+
+        if (n == 0) {
+            return 0;
+        }
+
+        int res = 0;
+        res = Fibonacci(n - 1) + Fibonacci(n - 2);
+
+        return res;
+
+    }
+}
