f96935a5f37dbce8f1d096e33f823762a26cc10a
XFour <837065279@qq.com>
2019-06-07 11:32:39 +0800

    剑指OFFER

--- Collection/10.4 变态跳台阶/src/Solution.java
+++ Collection/10.4 变态跳台阶/src/Solution.java
@@ -17,9 +17,11 @@ public class Solution {
 
         if (target == 0) {
             return 0;
+        } else if (target == 1) {
+            return 1;
         }
 
-        return 0;
+        return  2 * JumpFloorII(target - 1);
 
     }
 }
