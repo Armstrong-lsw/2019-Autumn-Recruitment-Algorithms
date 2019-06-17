d98ff31bff7cf07c1afbfd4229453e1b2dda7ede
XFour <837065279@qq.com>
2019-06-17 16:55:49 +0800

    剑指OFFER

--- Collection/4. 二维数组中的查找[RE0617]/src/Solution.java
+++ Collection/4. 二维数组中的查找[RE0617]/src/Solution.java
@@ -20,10 +20,30 @@
  */
 public class Solution {
 
+    public static void main(String[] args) {
+        int[][] matrix = new int[][]{{1, 4, 7, 11, 15}, {2, 5, 8, 12, 19}, {3, 6, 9, 16, 22}, {10, 13, 14, 17, 24}, {18, 21, 23, 26, 30}};
+        Solution solution = new Solution();
+        boolean find = solution.Find(9, matrix);
+        System.out.println(find);
+    }
+
     public boolean Find(int target, int[][] matrix) {
         if (matrix.length == 0 || matrix[0].length == 0) {
             return false;
         }
+        int i = 0, j = matrix[0].length - 1;
+        while (i < matrix.length && j >= 0) {
+            if (matrix[i][j] != target) {
+                if (matrix[i][j] < target) {
+                    i++;
+                } else {
+                    j--;
+                }
+            } else {
+                return true;
+            }
+        }
+        return false;
     }
 
 }
