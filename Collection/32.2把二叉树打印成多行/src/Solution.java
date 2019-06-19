3466d8484f81bd4b01f1af6b8787874b50cbda27
XFour <837065279@qq.com>
2019-06-19 22:51:06 +0800

    剑指OFFER

---
+++ Collection/32.2把二叉树打印成多行/src/Solution.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+/**
+ * Create with Collection
+ * User: 许清远
+ * Date: 2019/6/19
+ * Time: 22:44
+ * Description:
+ */
+public class Solution {
+
+    public class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+
+        public TreeNode(int val) {
+            this.val = val;
+
+        }
+
+    }
+
+    ArrayList<ArrayList<Integer>> Print(TreeNode pRoot) {
+        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+        if (pRoot == null) {
+            return res;
+        }
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.add(pRoot);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            ArrayList<Integer> list = new ArrayList<>();
+            while (size-- > 0) {
+                TreeNode node = queue.poll();
+                list.add(node.val);
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+            res.add(list);
+        }
+        return res;
+    }
+
+}
