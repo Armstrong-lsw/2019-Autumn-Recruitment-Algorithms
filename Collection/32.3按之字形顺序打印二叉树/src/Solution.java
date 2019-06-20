e961be5660d9fa953ed57cde8b50d0ddef73281d
XFour <837065279@qq.com>
2019-06-20 11:55:55 +0800

    剑指OFFER

--- Collection/32.3按之字形顺序打印二叉树/src/Solution.java
+++ Collection/32.3按之字形顺序打印二叉树/src/Solution.java
@@ -8,10 +8,20 @@ import java.util.LinkedList;
  * Date: 2019/6/19
  * Time: 22:52
  * Description:
+ * 请实现一个函数按照之字形打印二叉树，
+ * 即第一行按照从左到右的顺序打印，第二层按照从右至左的顺序打印，第三行按照从左到右的顺序打印，其他行以此类推。
  */
 public class Solution {
 
-    public class TreeNode {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(3);
+        ArrayList<ArrayList<Integer>> arrayLists = Print(root);
+        System.out.println("test");
+    }
+
+    public static class TreeNode {
         int val = 0;
         TreeNode left = null;
         TreeNode right = null;
@@ -23,7 +33,7 @@ public class Solution {
 
     }
 
-    public ArrayList<ArrayList<Integer> > Print(TreeNode pRoot) {
+    public static ArrayList<ArrayList<Integer> > Print(TreeNode pRoot) {
         ArrayList<ArrayList<Integer>> res = new ArrayList<>();
         if (pRoot == null) {
             return res;
@@ -37,19 +47,14 @@ public class Solution {
             while (size-- > 0) {
                 TreeNode node = queue.poll();
                 list.add(node.val);
-                if (leftFlag) {
-                    if (node.left != null)
-                        queue.add(node.left);
-                    if (node.right != null)
-                        queue.add(node.right);
-                } else {
-                    if (node.right != null)
-                        queue.add(node.right);
-                    if (node.left != null)
-                        queue.add(node.left);
-                }
+                if (node.left != null)
+                    queue.add(node.left);
+                if (node.right != null)
+                    queue.add(node.right);
             }
-            Collections.reverse(list);
+            if (!leftFlag) {
+                Collections.reverse(list);
+            }
             res.add(list);
             leftFlag = !leftFlag;
         }
