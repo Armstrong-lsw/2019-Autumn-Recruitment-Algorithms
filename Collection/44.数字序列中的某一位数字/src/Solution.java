6abaf5523e5c4944926f91a35ebb52d6e53deb86
XFour <837065279@qq.com>
2019-06-26 21:43:56 +0800

    剑指OFFER

---
+++ Collection/44.数字序列中的某一位数字/src/Solution.java
@@ -0,0 +1,10 @@
+/**
+ * Create with Collection
+ * User: 许清远
+ * Date: 2019/6/26
+ * Time: 21:38
+ * Description:
+ * 数字以 0123456789101112131415... 的格式序列化到一个字符串中，求这个字符串的第 index 位。
+ */
+public class Solution {
+}
