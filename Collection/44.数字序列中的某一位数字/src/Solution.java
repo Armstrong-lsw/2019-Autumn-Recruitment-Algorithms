08d79a235c509d14fc92ca9d1d3429d91fcce6bb
XFour <837065279@qq.com>
2019-06-29 20:21:01 +0800

    剑指OFFER

--- Collection/44.数字序列中的某一位数字/src/Solution.java
+++ Collection/44.数字序列中的某一位数字/src/Solution.java
@@ -7,4 +7,44 @@
  * 数字以 0123456789101112131415... 的格式序列化到一个字符串中，求这个字符串的第 index 位。
  */
 public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().getDigitAtIndex(19));
+    }
+
+    public int getDigitAtIndex(int index) {
+        if (index < 0) {
+            return -1;
+        }
+        int place = 1, totalNumber;
+        while (true) {
+            totalNumber = place * getAmountOfPlace(place);
+            if (index <= totalNumber) {
+                return getDigitAtIndex(index, place);
+            }
+            place++;
+            index -= totalNumber;
+        }
+    }
+
+    private int getAmountOfPlace(int place) {
+        if (place == 1) {
+            return 10;
+        }
+        return (int) Math.pow(10, place - 1);
+    }
+
+    private int getBeginNumberOfPlace(int place) {
+        if (place == 1) {
+            return 0;
+        }
+        return (int) Math.pow(10, place - 1);
+    }
+
+    private int getDigitAtIndex(int index, int place) {
+        int begin = getBeginNumberOfPlace(place);
+        String number = begin + index / place + "";
+        return number.charAt(index % place) - '0';
+    }
+
 }
