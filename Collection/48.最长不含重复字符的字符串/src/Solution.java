de35f20ccaecab366fd49f756b4591c50d4f7b44
XFour <837065279@qq.com>
2019-07-03 19:46:43 +0800

    剑指OFFER

---
+++ Collection/48.最长不含重复字符的字符串/src/Solution.java
@@ -0,0 +1,16 @@
+/**
+ * Create with Collection
+ * User: 许清远
+ * Date: 2019/7/3
+ * Time: 19:36
+ * Description:
+ * 输入一个字符串（只包含 a~z 的字符），求其最长不含重复字符的子字符串的长度。
+ * 例如对于 arabcacfr，最长不含重复字符的子字符串为 acfr，长度为 4。
+ */
+public class Solution {
+
+    public int longestSubStringWithoutDuplication(String str) {
+        return 0;
+    }
+
+}
