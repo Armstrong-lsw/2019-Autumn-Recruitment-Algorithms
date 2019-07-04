dcc04d6586ad1e3df9851ff33d2a31c707c2f92d
XFour <837065279@qq.com>
2019-07-04 17:04:24 +0800

    剑指OFFER

--- Collection/48.最长不含重复字符的字符串/src/Solution.java
+++ Collection/48.最长不含重复字符的字符串/src/Solution.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Create with Collection
  * User: 许清远
@@ -6,11 +8,36 @@
  * Description:
  * 输入一个字符串（只包含 a~z 的字符），求其最长不含重复字符的子字符串的长度。
  * 例如对于 arabcacfr，最长不含重复字符的子字符串为 acfr，长度为 4。
+ *
+ * 解题思路：https://blog.csdn.net/m0_37862405/article/details/80369128
  */
 public class Solution {
 
     public int longestSubStringWithoutDuplication(String str) {
-        return 0;
+
+        if (str == null || str.equals("")) {
+            return 0;
+        }
+
+        int[] positions = new int[26];
+        Arrays.fill(positions, -1);
+        int curLength = 0, maxLength = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            int index = str.charAt(i) - 'a';
+            int pos = positions[index];
+            if (pos == -1 || i - pos > curLength) {
+                curLength++;
+            } else {
+                maxLength = Math.max(maxLength, curLength);
+                curLength = i - pos;
+            }
+            positions[index] = i;
+        }
+        maxLength = Math.max(maxLength, curLength);
+
+        return maxLength;
+
     }
 
 }
