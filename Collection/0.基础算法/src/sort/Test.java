869ea500f9838959706c3da34ad025f77c053b37
XFour <837065279@qq.com>
2019-07-04 15:43:42 +0800

    剑指OFFER

--- Collection/0.基础算法/src/sort/Test.java
+++ Collection/0.基础算法/src/sort/Test.java
@@ -4,6 +4,7 @@ import 冒泡排序.BubbleSort;
 import 堆排序.HeapSort;
 import 希尔排序.ShellSort;
 import 归并排序.MergeSort;
+import 快速排序.QuickSort;
 import 插入排序.InsertionSort;
 import 选择排序.SelectionSort;
 
@@ -21,7 +22,9 @@ public class Test {
 //        SortTestHelper.isFinish(new BubbleSort());
 //        SortTestHelper.isFinish(new InsertionSort());
 //        SortTestHelper.isFinish(new MergeSort());
-        SortTestHelper.isFinish(new HeapSort());
+//        SortTestHelper.isFinish(new HeapSort());
+        SortTestHelper.isFinish(new QuickSort());
     }
+
 
 }
---
+++ Collection/0.基础算法/src/快速排序/QuickSort.java
@@ -0,0 +1,50 @@
+package 快速排序;
+
+import sort.Sort;
+
+/**
+ * Create with Collection
+ * User: 许清远
+ * Date: 2019/7/4
+ * Time: 15:25
+ * Description:
+ */
+public class QuickSort implements Sort {
+
+    @Override
+    public void sort(Integer[] nums) {
+        sort(nums, 0, nums.length - 1);
+    }
+
+    private void sort(Integer[] nums, int l, int r) {
+        if (l >= r) {
+            return;
+        }
+        int mid = partition(nums, l, r);
+        sort(nums, l, mid - 1);
+        sort(nums, mid + 1, r);
+    }
+
+    private int partition(Integer[] nums, int l, int r) {
+        swap(nums, l, (int) (Math.random() * (r - l + 1) + l));
+        Integer v = nums[l];
+        int i = l, j = r + 1;
+        while (true) {
+            while (i != r && nums[++i] < v);
+            while (j != l && nums[--j] > v);
+            if (i < j) {
+                swap(nums, i, j);
+            } else {
+                break;
+            }
+        }
+        swap(nums, l, j);
+        return j;
+    }
+
+    private void swap(Integer[] nums, int i, int j) {
+        Integer temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
