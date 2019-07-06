cddd54b5949c8c412307af91804cb7fc21dfe323
XFour <837065279@qq.com>
2019-07-06 18:50:36 +0800

    剑指OFFER

--- Collection/51.数组中的逆序对/src/Solution.java
+++ Collection/51.数组中的逆序对/src/Solution.java
@@ -25,7 +25,42 @@
  * 7
  */
 public class Solution {
+
+    //注意 long 值
+    private long cnt = 0;
+    private int[] tmp;
+
     public int InversePairs(int [] array) {
-        return 0;
+
+        if (array == null || array.length == 1) {
+            return 0;
+        }
+        tmp = new int[array.length];
+        sort(array, 0, array.length - 1);
+        return (int) (cnt % 1000000007);   //注意题目要求
+    }
+
+    private void sort(int[] arr, int l, int r) {
+        if (l >= r) {
+            return;
+        }
+        int mid = l + (r - l) / 2;
+        sort(arr, l, mid);
+        sort(arr,mid + 1, r);
+        merge(arr, l, mid, r);
+    }
+
+    private void merge(int[] arr, int l, int mid, int r) {
+        int i = l, j = mid + 1;
+        for (int k = l; k <= r; k++) {
+            if (i > mid) tmp[k] = arr[j++];
+            else if (j > r) tmp[k] = arr[i++];
+            else if (arr[i] < arr[j]) tmp[k] = arr[i++];
+            else {
+                tmp[k] = arr[j++];
+                cnt += mid - i + 1;
+            }
+        }
+        System.arraycopy(tmp, l, arr, l, r - l + 1);
     }
 }
