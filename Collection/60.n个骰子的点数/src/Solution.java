f49e2b72e234bbbf9701e537fe48f8c0069836e7
XFour <837065279@qq.com>
2019-07-08 16:28:53 +0800

    剑指OFFER

---
+++ Collection/60.n个骰子的点数/src/Solution.java
@@ -0,0 +1,17 @@
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Create with Collection
+ * User: 许清远
+ * Date: 2019/7/8
+ * Time: 16:19
+ * Description:把 n 个骰子仍在地上，求点数和为 s 的概率。
+ */
+public class Solution {
+
+    public List<Map.Entry<Integer, Double>> dicesSum(int n) {
+        return null;
+    }
+
+}
