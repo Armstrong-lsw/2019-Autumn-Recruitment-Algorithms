58800822d1030cde507f8e14466ea6bf4e98cba5
XFour <837065279@qq.com>
2019-07-14 14:10:35 +0800

    剑指OFFER

--- Collection/38.字符串排序[RE0714]/src/Solution.java
+++ Collection/38.字符串排序[RE0714]/src/Solution.java
@@ -4,11 +4,9 @@ import java.util.Arrays;
 /**
  * Create with Collection
  * User: 许清远
- * Date: 2019/6/21
- * Time: 22:36
+ * Date: 2019/7/14
+ * Time: 12:41
  * Description:
- * 输入一个字符串,按字典序打印出该字符串中字符的所有排列。
- * 例如输入字符串abc,则打印出由字符a,b,c所能排列出来的所有字符串abc,acb,bac,bca,cab和cba。
  */
 public class Solution {
 
@@ -18,27 +16,25 @@ public class Solution {
         if (str == null || str.equals("")) return list;
         char[] chars = str.toCharArray();
         Arrays.sort(chars);
-        PermutationHandler(chars, new boolean[chars.length], new StringBuilder());
+        Permutation(chars,new boolean[chars.length],new StringBuilder());
         return list;
     }
 
-    private void PermutationHandler(char[] chars, boolean[] hasUsed, StringBuilder s) {
-
-        if (s.length() == chars.length) {
+    private void Permutation(char[] chars, boolean[] hasUsed, StringBuilder s) {
+        if (chars.length == s.length()) {
             list.add(s.toString());
             return;
         }
-
         for (int i = 0; i < chars.length; i++) {
             if (hasUsed[i]) continue;
             if (i != 0 && chars[i] == chars[i-1] && !hasUsed[i-1]) continue;
             s.append(chars[i]);
             hasUsed[i] = true;
-            PermutationHandler(chars, hasUsed, s);
-            s.deleteCharAt(s.length() - 1);
+            Permutation(chars, hasUsed, s);
             hasUsed[i] = false;
+            s.deleteCharAt(s.length() - 1);
         }
-
     }
+
 
 }
