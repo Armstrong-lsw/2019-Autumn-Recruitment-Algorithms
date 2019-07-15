88c4abb73c0110eb0fb699f45c591e3a5bffc4c0
XFour <837065279@qq.com>
2019-07-15 11:52:37 +0800

    剑指OFFER

---
+++ Collection/40.最小的K个数[RE0715]/src/Solution.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+
+/**
+ * Create with Collection
+ * User: 许清远
+ * Date: 2019/7/15
+ * Time: 11:04
+ * Description:
+ */
+public class Solution {
+
+    public ArrayList<Integer> GetLeastNumbers_Solution(int [] input, int k) {
+        ArrayList<Integer> list = new ArrayList<>();
+        if (input == null || input.length < k || k <= 0) {
+            return list;
+        }
+        findKthSmallest(input, k - 1);
+        for (int i = 0; i < k; i++) {
+            list.add(input[i]);
+        }
+        return list;
+    }
+
+    private void findKthSmallest(int[] nums, int k) {
+        int l = 0, r = nums.length - 1;
+        while (true){
+            int p = partition(nums, l, r);
+            if (p == k) {
+                break;
+            } else if (p < k) {
+                l = p + 1;
+            } else {
+                r = p - 1;          //这里注意！！！不能直接等于p
+            }
+        }
+    }
+
+    private int partition(int[] nums, int l, int r) {
+        int v = nums[l];
+        int i = l, j = r + 1;
+        while (true){
+            while (i != r && nums[++i] < v);
+            while (j != l && nums[--j] > v);
+            if (i >= j) {
+                break;
+            }
+            swap(nums, i, j);
+        }
+        swap(nums, l, j);
+        return j;
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+
+}
