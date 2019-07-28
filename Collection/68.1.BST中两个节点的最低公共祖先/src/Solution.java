2cb1d7e7d59b03934fb020d3e0bd2e54538c4114
XFour <837065279@qq.com>
2019-07-28 23:54:45 +0800

    剑指OFFER

---
+++ Collection/68.1.BST中两个节点的最低公共祖先/src/Solution.java
@@ -0,0 +1,26 @@
+/**
+ * @author 许清远
+ * @date 2019/7/28
+ */
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root.val == p.val || root.val == q.val) {
+            return root;
+        }
+        if (p.val < root.val && q.val < root.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        }
+        if (p.val > root.val && q.val > root.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+        return root;
+    }
+}
