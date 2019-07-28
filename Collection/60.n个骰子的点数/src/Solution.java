9d3070e07878b615a55bd2462c5fb9b93a3c1b87
XFour <837065279@qq.com>
2019-07-28 20:31:34 +0800

    剑指OFFER

--- Collection/60.n个骰子的点数/src/Solution.java
+++ Collection/60.n个骰子的点数/src/Solution.java
@@ -1,3 +1,5 @@
+import java.util.AbstractMap;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -9,9 +11,32 @@ import java.util.Map;
  * Description:把 n 个骰子仍在地上，求点数和为 s 的概率。
  */
 public class Solution {
-
+    /**
+     * @param n an integer
+     * @return a list of Map.Entry<sum, probability>
+     */
     public List<Map.Entry<Integer, Double>> dicesSum(int n) {
-        return null;
+        List<Map.Entry<Integer, Double>> res = new ArrayList<>();
+        if (n < 1) {
+            return res;
+        }
+        int faces = 6;
+        int maxNum = faces * n;
+        long[][] dp = new long[n + 1][maxNum + 1];
+        for (int i = 1; i <= faces; i++) {
+            dp[1][i] = 1;
+        }
+        for (int i = 2; i <= n; i++) {
+            for (int j = i; j <= maxNum; j++) {
+                for (int k = 1; k <= faces && k <= j; k++) {
+                    dp[i][j] += dp[i-1][j - k];
+                }
+            }
+        }
+        double totalNum = Math.pow(6, n);
+        for (int i = n; i <= maxNum; i++) {
+            res.add(new AbstractMap.SimpleEntry<>(i, dp[n][i] / totalNum));
+        }
+        return res;
     }
-
 }
