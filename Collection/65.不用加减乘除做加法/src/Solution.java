91b7444d39c34023b0b03c585bf6a3e5acd5f816
XFour <837065279@qq.com>
2019-07-28 21:47:07 +0800

    剑指OFFER

---
+++ Collection/65.不用加减乘除做加法/src/Solution.java
@@ -0,0 +1,9 @@
+/**
+ * @author 许清远
+ * @date 2019/7/28
+ */
+public class Solution {
+    public int Add(int num1,int num2) {
+        return num2 == 0 ? num1 : Add(num1 ^ num2, (num1 & num2) << 1);
+    }
+}
